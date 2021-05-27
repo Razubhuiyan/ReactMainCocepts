6f51df16268441add05e440a591c8a45b9f18d74
Razu Bhuiyan <razubn1994@gmail.com>
2021-05-27 15:43:00 +0600

    Introduction to jsx

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+// import React from 'react';
+// Introduction to jsx
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function formatname(user) {
+    return `${user.firstName} ${user.lastName}`;
+}
+
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatname(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger</h1>;
+}
+
+const user = {
+    firstName: 'Razu',
+    lastName: 'Bhuiyan',
+};
+
+const element = <h1>{getGreeting(user)}</h1>;
+
+ReactDOM.render(element, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
